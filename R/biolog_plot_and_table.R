d4c908a56251c2730a13b57dfed4a8a69c3e09a9
Jeff Kimbrel <jakpot@gmail.com>
2018-09-05 19:15:08 -0700

    v0.3.8

--- R/biolog_plot_and_table.R
+++ R/biolog_plot_and_table.R
@@ -3,6 +3,8 @@
 #' @param x A df output from read_multiplate_biolog_experiment()
 #' @param w A wavelength (or vector of wavelengths)
 #'
+#' Wavelength to RGB mappings come from https://academo.org/demos/wavelength-to-colour-relationship/
+#'
 #' @export
 
 biolog_plot_and_table = function(x, w = '600') {
@@ -22,14 +24,14 @@ biolog_plot_and_table = function(x, w = '600') {
 
   p = ggplot(df.s, aes(x = TIME, y = value)) +
     jak_theme() +
-    geom_point(size = 2, alpha = 0.8, aes(color = Wavelength)) +
     geom_line(aes(group = Wavelength, color = Wavelength)) +
-    facet_wrap(~Substrate, nrow = 12) +
-    scale_color_manual(values = c("600" = "#BEAD93", "450" = "#A15767", "750" = "#003388")) +
+    geom_point(size = 3, alpha = 0.9, pch = 21, aes(fill = Wavelength)) +
+    facet_grid(Row~Column) +
+    scale_fill_manual(values = c(`600` = "#ffbe00", `450` = "#0046ff", `750` = "#a10000")) +
+    scale_color_manual(values = c(`600` = "#ffbe00", `450` = "#0046ff", `750` = "#a10000")) +
     geom_point(data = dfWithNeg, aes(x = TIME, y = value, color = Wavelength), size = 1, pch = 3) +
     geom_line(data = dfWithNeg, aes(group = Wavelength, color = Wavelength), linetype = "dashed") +
-    ggtitle("Solid line = Substrate, Dashed = Negative Control") +
-    theme(strip.text.x = element_text(size = 7))
+    ggtitle("Solid line = Substrate, Dashed = Negative Control")
 
   tb = df.s %>%
     select(Substrate, Wavelength, TIME, value) %>%
